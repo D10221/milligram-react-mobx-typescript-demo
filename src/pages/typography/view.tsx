613a632e36df58cc494dde64962f249f1559f570
D10221 <danieliozzi@gmail.com>
2017-05-14 08:39:10 +0200

    webpack: prepare src/... to require text

--- src/pages/typography/view.tsx
+++ src/pages/typography/view.tsx
@@ -1,14 +1,16 @@
 import * as React from "react";
 import { observer } from "mobx-react";
 
-import { Container, } from "../elements/container";
-import { Title3 } from "../elements/headers";
-import { Row } from "../elements/row";
-import { Column } from "../elements/column";
-import { Description } from "../elements/description";
-import { CodePrettyprint } from "../elements/code-prettyprint";
-import { CodeContent } from "../elements/code-content";
-import { styles } from "./typography.style";
+import { Container, } from "../../elements/container";
+import { Title3 } from "../../elements/headers";
+import { Row } from "../../elements/row";
+import { Column } from "../../elements/column";
+import { Description } from "../../elements/description";
+import { CodePrettyprint } from "../../elements/code-prettyprint";
+import { CodeContent } from "../../elements/code-content";
+import { styles } from "./style";
+
+import * as code from "./code.html!text";
 
 const HeadingFont = (props: React.HTMLProps<HTMLSpanElement>) => (
     <span style={styles.headingFontSize} {...props} />
@@ -33,29 +35,7 @@ export const Page = observer(() => {
             </Row>
             <CodePrettyprint>
                 <CodeContent>
-                    {`
-<!-- Base font-size and line-height -->
-<p>The base type is 1.6rem (16px) over 1.6 line height (24px)</p>= '\n\n'
-<!-- Other elements to text markup -->
-<a>Anchor</a>
-<em>Emphasis</em>
-<small>Small</small>
-<strong>Strong</strong>
-<u>Underline</u>
-= \n\n
-<!-- Default Headings -->
-<h1>Heading</h1>
-<h2>Heading</h2>
-<h3>Heading</h3>
-<h4>Heading</h4>
-<h5>Heading</h5>
-<h6>Heading</h6>
-= \n\n
-<!-- The base font-size is set at 62.5%\n
-for having the convenience of sizing rems\n
-in a way that is similar to using px.\n
-So basically 1.6rem = 16px. -->\n
-`}
+                    {code}
                 </CodeContent>
             </CodePrettyprint>
         </Container>
