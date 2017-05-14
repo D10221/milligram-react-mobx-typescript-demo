e01383c93d7653bf547641b6b23fb52d738102ea
D10221 <danieliozzi@gmail.com>
2017-05-14 06:49:37 +0200

    pages :typography

---
+++ src/pages/typography.style.ts
@@ -0,0 +1,7 @@
+export const styles: React.CSSProperties = {
+    headingFontSize: {
+        color: "#999",
+        fontSize: "1.2rem",
+        letterSpacing: "normal"
+    }
+};
--- src/pages/typography.tsx
+++ src/pages/typography.tsx
@@ -8,38 +8,27 @@ import { Column } from "../elements/column";
 import { Description } from "../elements/description";
 import { CodePrettyprint } from "../elements/code-prettyprint";
 import { CodeContent } from "../elements/code-content";
+import { styles } from "./typography.style";
+
+const HeadingFont = (props: React.HTMLProps<HTMLSpanElement>) => (
+    <span style={styles.headingFontSize} {...props} />
+);
 export const Page = observer(() => {
+    // ...
     return (
         <Container>
             <Title3>Typography</Title3>
             <Description>
                 ... description
-                    ... description
-                    ... description
                 </Description>
             <Row>
                 <Column>
-                    <h1>Heading </h1><span className="heading-font-size">
-                        <code>h1 4.6rem (46px)</code>
-                    </span>
-                    <h2>Heading</h2><span className="heading-font-size">
-                        <code>h2 3.6rem (36px)</code>
-                    </span>
-                    <h3>Heading<span className="heading-font-size">
-                        <code>h3 2,8rem (28px)</code>
-                    </span>
-                    </h3>
-                    <h4>Heading</h4><span className="heading-font-size">
-                        <code>h4 2.2rem (22px)</code>
-                    </span>
-                    <h5>Heading</h5>
-                    <span className="heading-font-size">
-                        <code>h5 1.8rem (18px)</code>
-                    </span>
-                    <h6>Heading</h6>
-                    <span className="heading-font-size">
-                        <code>h6 1.6rem (16px)</code>
-                    </span>
+                    <h1>Heading<HeadingFont> <code>h1</code> 4.6rem (46px)</HeadingFont></h1>
+                    <h2>Heading<HeadingFont> <code>h2</code> 3.6rem (36px)</HeadingFont></h2>
+                    <h3>Heading<HeadingFont> <code>h3</code> 2.8rem (28px)</HeadingFont></h3>
+                    <h4>Heading<HeadingFont> <code>h4</code> 2.2rem (22px)</HeadingFont></h4>
+                    <h5>Heading<HeadingFont> <code>h5</code> 1.8rem (18px)</HeadingFont></h5>
+                    <h6>Heading<HeadingFont> <code>h6</code> 1.6rem (16px)</HeadingFont></h6>
                 </Column>
             </Row>
             <CodePrettyprint>
@@ -73,6 +62,3 @@ So basically 1.6rem = 16px. -->\n
     );
 }
 );
-
-/*
- */
